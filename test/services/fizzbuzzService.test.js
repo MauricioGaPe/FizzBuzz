8aef6a0802a54554bf3fd4279b2378aeb91e2e0e
Mauricio García Pérez <ing.magape.fa@gmail.com>
2022-05-02 12:29:24 -0500

    Se sube todo el código inicial

---
+++ test/services/fizzbuzzService.test.js
@@ -0,0 +1,19 @@
+const FizzBuzzService = require('../../lib/services/fizzbuzzService');
+const Reader = require("../../lib/utils/reader")
+
+describe('Test para FizzBuzzService', () => {
+    test('Método applyValidationInExplorer', () => {
+        const applyValidationInExplorer = FizzBuzzService.applyValidationInExplorer(Reader.readJsonFile("explorers.json"), "node");
+        applyValidationInExplorer.forEach(explorer => {
+            if(explorer.score%5 === 0 && explorer.score%3 === 0){
+                expect(explorer.trick).toBe("FIZZBUZZ");
+            } else if(explorer.score%3 === 0){
+                expect(explorer.trick).toBe("FIZZ");
+            } else if(explorer.score%5 === 0){
+                expect(explorer.trick).toBe("BUZZ");
+            } else {
+                expect(explorer.trick).toBe(explorer.score);
+            }
+        });
+    });
+});
